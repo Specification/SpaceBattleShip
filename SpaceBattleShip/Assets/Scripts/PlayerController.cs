1220deecd2e7ffb87dc5f9705ac9c684c87d594c
LAPTOP-H7OTTR3I\Owner <fall10241024@gmail.com>
2018-11-01 16:05:49 +0900

    戦闘画面においてのカウントダウンとSEのスクリプトの作成

---
+++ SpaceBattleShip/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float m_maxHP = 100f;      //最大HP
+    float m_hp;             //HP
+
+    HPBar m_Hpbar;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        m_Hpbar = GetComponent<HPBar>();
+
+        m_hp = 100f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        m_hp -= 10f;
+        if(m_hp<0)
+        {
+            m_hp = 0f;
+        }
+
+        m_Hpbar.Set(m_hp / m_maxHP);
+	}
+}
---
+++ SpaceBattleShip/Assets/Scripts/TimerController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController: MonoBehaviour
+{
+    public Text timerText;
+
+    public float totalTime;
+    int seconds;
+
+    public float destroyTime;
+
+    public PlayerController PL1;
+    public PlayerController PL2;
+    // Use this for initialization
+    void Start ()
+    {
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+        if (totalTime>0)
+        {
+            totalTime -= Time.deltaTime;
+            seconds = (int)totalTime+1;
+            
+            if (totalTime <= 0)
+            {
+                //PL1.enabled = true;
+                //PL2.enabled = true;
+                timerText.text = "スタート";
+            }
+            else
+            {
+                //PL1.enabled = false;
+                //PL2.enabled = false;
+                timerText.text = seconds.ToString();
+            }
+
+        }
+
+        destroyTime -= Time.deltaTime;
+
+        if (destroyTime <= 0)
+        {
+            Destroy(timerText);
+        }
+    }
+}
